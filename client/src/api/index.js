45960d376f78f17df634b116727b3b49313e05ad
davidkiama <davidkiama98@gmail.com>
2022-06-19 19:30:57 +0300

    Added the deployed api link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/transaction/";
+// const API_URL = "http://localhost:5000/transaction/";
+const API_URL = "https://fathomless-cove-25106.herokuapp.com/";
 
 export const createCustomer = (data) => axios.post(`${API_URL}customer`, data);
 export const createCheckout = (data) => axios.post(`${API_URL}checkout`, data);
