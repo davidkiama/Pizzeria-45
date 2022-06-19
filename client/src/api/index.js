98dc603d9999782813066b143e1b0001d3b01ba9
davidkiama <davidkiama98@gmail.com>
2022-06-19 19:46:57 +0300

    Added transaction url prefix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:5000/transaction/";
-const API_URL = "https://fathomless-cove-25106.herokuapp.com/";
+const API_URL = "https://fathomless-cove-25106.herokuapp.com/transaction/";
 
 export const createCustomer = (data) => axios.post(`${API_URL}customer`, data);
 export const createCheckout = (data) => axios.post(`${API_URL}checkout`, data);
